39f124df388105e9e3e092359e6a59e911e6d9a6
Sanjay Shah <Sanjay Shah>
2018-03-05 11:34:27 -0800

    Initial Commit

---
+++ FairFeesApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FairFeesApp
+//
+//  Created by Sanjay Shah on 2018-03-05.
+//  Copyright © 2018 Fair Fees. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
