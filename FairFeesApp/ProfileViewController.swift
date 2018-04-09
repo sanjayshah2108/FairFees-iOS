ecba40048b8c1f86ac88b58e78702df20e62e174
Sanjay Shah <Sanjay Shah>
2018-04-09 13:51:29 -0400

    can now remove or deactivate a listing

--- FairFeesApp/ProfileViewController.swift
+++ FairFeesApp/ProfileViewController.swift
@@ -16,12 +16,26 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
    
 
     var listingsTableView: UITableView!
+    var myHomeSales: [HomeSale]!
+    var myHomeRentals: [HomeRental]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "Profile"
         view.backgroundColor = UIColor.white
+        
+        myHomeSales = []
+        myHomeRentals = []
+        
+        for listing in (FirebaseData.sharedInstance.currentUser?.listings)! {
+            if (listing is HomeSale){
+                myHomeSales.append(listing as! HomeSale)
+            }
+            else if (listing is HomeRental){
+                myHomeRentals.append(listing as! HomeRental)
+            }
+        }
         
         setupProfileLabels()
      
@@ -61,7 +75,7 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         listingsTableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(listingsTableView)
         
-        listingsTableView.register(UINib(nibName: "ProfileListingsTableViewCell", bundle: nil), forCellReuseIdentifier: "profileListingTableViewCell")
+        listingsTableView.register(UINib(nibName: "HomeTableViewCell", bundle: nil), forCellReuseIdentifier: "homeTableViewCell")
     }
     
     func setupConstraints(){
@@ -80,19 +94,72 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         NSLayoutConstraint(item: listingsTableView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (FirebaseData.sharedInstance.currentUser?.listings.count)!
+        switch section
+        {
+        case 0:
+            return myHomeSales.count
+           // return (FirebaseData.sharedInstance.currentUser?.listings.filter { $0 is HomeSale }.count)!
+        case 1:
+            return myHomeRentals.count
+            //return (FirebaseData.sharedInstance.currentUser?.listings.filter { $0 is HomeRental }.count)!
+        default:
+            return 0
+        }
+        
+        //return (FirebaseData.sharedInstance.currentUser?.listings.count)!
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+    {
+        switch section
+        {
+        case 0:
+            return "For Sale"
+        case 1:
+            return "For Rent"
+        default:
+            return "Other"
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "profileListingTableViewCell") as! ProfileListingsTableViewCell
+        //Use ProfileListingsTableViewCell if we need a different format
+        let cell = tableView.dequeueReusableCell(withIdentifier: "homeTableViewCell") as! HomeTableViewCell
         
-        cell.listingNameLabel.text = FirebaseData.sharedInstance.currentUser?.listings[indexPath.row].name
-        cell.listingImageView.sd_setImage(with: Storage.storage().reference().child((FirebaseData.sharedInstance.currentUser?.listings[indexPath.row].photoRefs[0])!))
-        cell.listingImageView.contentMode = .scaleAspectFill
-        cell.listingImageView.clipsToBounds = true
+        if(indexPath.section == 0){
+            
+            let homeSale = myHomeSales[indexPath.row]
+            
+            cell.bedroomsLabel.text = String(homeSale.bedroomNumber)
+            cell.bathroomsLabel.text = String(homeSale.bathroomNumber)
+            cell.priceLabel.text = String(homeSale.price)
+            cell.nameLabel.text = homeSale.name
+            cell.addressLabel.text = homeSale.address
+            cell.sizeLabel.text = String((homeSale.size)!)
+            cell.leftImageView.sd_setImage(with: Storage.storage().reference().child((homeSale.photoRefs[0])))
+        }
+        if(indexPath.section == 1){
+            let homeRental = myHomeRentals[indexPath.row]
+            
+            cell.bedroomsLabel.text = String(homeRental.bedroomNumber)
+            cell.bathroomsLabel.text = String(homeRental.bathroomNumber)
+            cell.priceLabel.text = String(homeRental.monthlyRent)
+            cell.nameLabel.text = homeRental.name
+            cell.addressLabel.text = homeRental.address
+            cell.sizeLabel.text = String((homeRental.size)!)
+            cell.leftImageView.sd_setImage(with: Storage.storage().reference().child((homeRental.photoRefs[0])))
+        }
+        
+    
+        cell.leftImageView.contentMode = .scaleAspectFill
+        cell.leftImageView.clipsToBounds = true
+        
         
         return cell
     }
@@ -108,5 +175,81 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete){
+            var listingToEdit: Listing? = nil
+            if(indexPath.section == 0){
+                listingToEdit = myHomeSales[indexPath.row]
+            }
+            else if(indexPath.section == 1){
+                listingToEdit = myHomeRentals[indexPath.row]
+            }
+            
+           deleteListing(listing: listingToEdit!, indexPath: indexPath)
+        }
+        
+        
+    }
+    
+//    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+//        return "Remove"
+//    }
+    
+    func deleteListing(listing: Listing, indexPath: IndexPath){
+        
+        var homeSaleToEdit: HomeSale? = nil
+        var homeRentalToEdit: HomeRental? = nil
+        
+        if(indexPath.section == 0){
+            homeSaleToEdit = listing as? HomeSale
+        }
+        else if(indexPath.section == 1){
+             homeRentalToEdit = listing as? HomeRental
+        }
+        
+        let deleteOrDeactivateAlert = UIAlertController(title: "What would you like to do?", message: "You could choose to either deactivate or delete your listing. If you deactivate, you can re-enlist it again. If you delete, you can't reverse that", preferredStyle: .alert)
+        
+        let deactivateAction = UIAlertAction(title: "Deactivate this listing", style: .default, handler:{ (action) in
+            listing.active = false
+            
+            if(indexPath.section == 0){
+                WriteFirebaseData.writeHomesForSale(homeForSale: homeSaleToEdit!)
+            }
+            else if(indexPath.section == 1){
+                WriteFirebaseData.writeHomesForRent(homeForRent: homeRentalToEdit!)
+            }
+            
+            let cell = self.listingsTableView.cellForRow(at: indexPath) as! HomeTableViewCell
+            //change this to strikethrough
+            cell.priceLabel.text = "Deactive"
+        })
+        let deleteAction = UIAlertAction(title: "Delete this listing", style: .default, handler: { (action) in
+            
+            print("Delete")
+            self.listingsTableView.reloadData()
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        deleteOrDeactivateAlert.addAction(deactivateAction)
+        deleteOrDeactivateAlert.addAction(deleteAction)
+        deleteOrDeactivateAlert.addAction(cancelAction)
+        
+        present(deleteOrDeactivateAlert, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let deleteButton = UITableViewRowAction(style: .default, title: "Remove") { (action, indexPath) in
+           tableView.dataSource?.tableView!(tableView, commit: .delete, forRowAt: indexPath)
+            return
+        }
+        deleteButton.backgroundColor = UIColor.black
+        return [deleteButton]
+    }
+    
+    func reActivateListing(listing: Listing, indexPath: IndexPath){
+        
     }
 }
